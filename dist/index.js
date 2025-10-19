8bddd87dd1505bdc1bbf256a7075e0820a3c3743
Youth Development Group <youthdev10313@gmail.com>
2025-10-19 19:04:36 +0330

    correct export

--- dist/index.js
+++ dist/index.js
@@ -15,3 +15,11 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./lib"), exports);
+try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require.resolve("react");
+    Object.assign(exports, require("./react/index"));
+}
+catch {
+    // React not installed – ignore
+}
