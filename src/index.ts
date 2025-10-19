b370aba18d0ced60a56a919983a40e5d281923d9
Youth Development Group <youthdev10313@gmail.com>
2025-10-19 19:04:13 +0330

    correct export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export * from './lib';
+try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require.resolve("react");
+    Object.assign(exports, require("./react/index"));
+} catch {
+    // React not installed – ignore
+}
